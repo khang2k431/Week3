3ab5220760e7218a0a46e23bdfc03dac70b55d70
khang2k431 <khangtpdce180788@fpt.edu.vn>
2025-09-17 15:41:37 +0700

    Create redis.go

---
+++ util/redis.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+var Rdb *redis.Client
+var ctx = context.Background()
+
+func InitRedis() {
+	Rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+	_, err := Rdb.Ping(ctx).Result()
+	if err != nil {
+		panic("❌ Redis not connected")
+	}
+	fmt.Println("✅ Redis connected")
+}
+
+// Presence tracking
+func SetUserOnline(user string) {
+	Rdb.Set(ctx, "presence:"+user, "online", 0)
+}
+
+func SetUserOffline(user string) {
+	Rdb.Del(ctx, "presence:" + user)
+}
