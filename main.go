eea27cd9bd83ca30e48558545561b2bd501799c1
khang2k431 <khangtpdce180788@fpt.edu.vn>
2025-09-17 15:34:23 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,49 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"github.com/khang2k431/Week3/config"
-	"github.com/khang2k431/Week3/controllers"
-	"github.com/khang2k431/Week3/middlewares"
-	"github.com/khang2k431/Week3/models"
+	"net/http"
+	"realtime-chat/api"
+	"realtime-chat/db"
+	"realtime-chat/util"
 )
 
 func main() {
-	_ = godotenv.Load()
-	config.InitDB()
+	// Kết nối database (giả sử PostgreSQL/MySQL)
+	db.ConnectDB()
 
-	if config.DB == nil {
-		log.Fatal("Database connection failed")
-	}
+	// Kết nối Redis
+	util.InitRedis()
 
-	// migrate
-	config.DB.AutoMigrate(&models.User{})
+	// Tạo router
+	r := api.SetupRouter()
 
-	r := gin.Default()
-
-	// Public routes
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
-
-	// Protected
-	auth := r.Group("/api")
-	auth.Use(middlewares.JWTAuthMiddleware())
-	{
-		auth.GET("/profile", func(c *gin.Context) {
-			claims, _ := c.Get("claims")
-			c.JSON(200, gin.H{"claims": claims})
-		})
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	log.Println("Server running at http://localhost:" + port)
-	r.Run(":" + port)
+	fmt.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
+
